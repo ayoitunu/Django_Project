9ae1627310891427d9b1fad057262faf511fe3c9
ayoitunu <ayoitunu@yahoo.com>
2019-05-14 14:53:43 +0100

    Book commit

---
+++ Book.java
@@ -0,0 +1,11 @@
+public class Book{
+    private int numberOfPages;
+    private String author;
+    private String title;
+    private String genre;
+    private boolean isHardCover;
+
+
+
+    
+}
