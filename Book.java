b5128be0603d2197fed2a36e8827128f439832e0
ayoitunu <ayoitunu@yahoo.com>
2019-05-14 16:07:45 +0100

    first updated Book commit

--- Book.java
+++ Book.java
@@ -1,9 +1,42 @@
 public class Book{
-    private int numberOfPages;
+    private int numOfPages;
     private String author;
     private String title;
     private String genre;
     private boolean isHardCover;
+
+    public void setNumOfPages(int numOfPages){
+        this.numOfPages = numOfPages;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+    public void setIsHardCover(){
+        this.isHardCover = true;
+    }
+    public int getNumOfPages(){
+        return numOfPages;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getGenre(){
+        return genre;
+    }
+    public boolean getIsHardCover(){
+        return isHardCover;
+    }
+
+        
 
 
 
